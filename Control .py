ad588fadc80ff1e7bee662326c08bef39080374c
joshuampenafiel <joshuampenafiel@gmail.com>
2026-02-14 10:18:06 -0500

    Add files via upload
    W/o Multithreading

---
+++ Control .py
@@ -0,0 +1,4 @@
+import os
+exec(open(os.path.expanduser('~'),"Projects","Rocketry","Telemetry","Telemetry.py").read()) 
+exec(open(os.path.expanduser('~'),"Projects","Rocketry","Camera","camera.py").read())
+print("done")
