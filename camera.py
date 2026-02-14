4fabf848a724560e7fc9da6cdd370081700c6c36
joshuampenafiel <joshuampenafiel@gmail.com>
2026-02-14 11:02:49 -0500

    Add files via upload
    Put the telemetry file call in camera

--- camera.py
+++ camera.py
@@ -83,6 +83,7 @@ while True:
         )
         cv2.imwrite(frame_filename, frame)
         print(f"Saved {frame_filename}")
+        exec(open(os.path.join(os.path.expanduser('~'),"Projects","Rocketry","Telemetry","Telemetry.py")).read()) 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
